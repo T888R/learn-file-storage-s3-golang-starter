e5e8fa7fdc3b99ebf524aaf6dda1477ba3d979ba
T8 <108910965+T888R@users.noreply.github.com>
2025-03-14 18:03:32 -0600

    added thumbnail caching

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	// "encoding/base64"
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -12,6 +17,16 @@ import (
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
+
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Could not read random bytes", err)
+		return
+	}
+	randomURL := base64.RawURLEncoding.EncodeToString(key)
+	fmt.Println(randomURL)
+
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
@@ -45,13 +60,30 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	defer file.Close()
 
 	contentType := header.Header.Get("Content-Type")
-	fmt.Println(contentType)
-
-	data, err := io.ReadAll(file)
+	mediaType, _, err := mime.ParseMediaType(contentType)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
 		return
 	}
+
+	fmt.Println(mediaType)
+
+	var fileExtension string
+
+	if contentType == "image/png" {
+		fileExtension = ".png"
+	} else if contentType == "image/jpeg" {
+		fileExtension = ".jpeg"
+	} else {
+		respondWithError(w, http.StatusBadRequest, "Not a jpeg or a png", err)
+		return
+	}
+
+	// data, err := io.ReadAll(file)
+	// if err != nil {
+	// 	respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+	// 	return
+	// }
 
 	videoData, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -64,21 +96,42 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	encodedData := base64.StdEncoding.EncodeToString(data)
-	thumbnail_url := fmt.Sprintf("data:%s;base64,%s", contentType, encodedData)
+	// encodedData := base64.StdEncoding.EncodeToString(data)
+	// thumbnail_url := fmt.Sprintf("data:%s;base64,%s", contentType, encodedData)
+	// videoData.ThumbnailURL = &thumbnail_url
+
+	// fileURL := fmt.Sprintf("/assets/%s.%s", videoID, fileExtension)
+
+	fileName := randomURL + fileExtension
+	fileURL := filepath.Join(cfg.assetsRoot, fileName)
+
+	thumbnailFile, err := os.Create(fileURL)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not save to file url", err)
+		return
+	}
+	defer thumbnailFile.Close()
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not reset file position", err)
+		return
+	}
+
+	bytesCopied, err := io.Copy(thumbnailFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Copy to file failed", err)
+		return
+	}
+
+	fmt.Println("Copied bytes", bytesCopied)
+	fmt.Println(fileURL)
+
+	thumbnail_url := fmt.Sprintf("http://localhost:%s/assets/%s%s", cfg.port, randomURL, fileExtension)
+
+	fmt.Println(thumbnail_url)
+
 	videoData.ThumbnailURL = &thumbnail_url
-
-	// fmt.Println(thumbnail_url)
-
-	// videoThumb := thumbnail{
-	// 	data:      data,
-	// 	mediaType: contentType,
-	// }
-	//
-	// videoThumbnails[videoID] = videoThumb
-	//
-	// thumbURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
-	// videoData.ThumbnailURL = &thumbURL
 
 	err = cfg.db.UpdateVideo(videoData)
 	if err != nil {
